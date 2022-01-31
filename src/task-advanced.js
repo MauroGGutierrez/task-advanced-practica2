4371761cff52f9e7c126eb842ed22661d0a83f74
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-01-31 13:54:00 -0300

    empezando

---
+++ src/task-advanced.js
@@ -0,0 +1,21 @@
+// import React, { useState } from 'react';
+import React from 'react';
+import "./taskadvanced.css";
+
+function TaskAdvanced() {
+  // const [array, setArray] = useState([]);
+  // const [enter, setEnter] = useState("");
+  // const addButton = (e) => {
+  //   e.preventDefault();
+  //   setEnter("");
+  //   setArray([...array, enter]);
+  // };
+
+  return (
+    <div className="Task-ad">
+      <h1>Task Advanced</h1>
+    </div>
+  );
+};
+
+export default TaskAdvanced();
